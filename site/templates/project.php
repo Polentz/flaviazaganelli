d225fcace48fb2c6122f011b51e026050b6f069c
Giulia Polenta <giuliapolenta@gmail.com>
2023-08-14 16:39:00 +0200

    add and style buttons

--- site/templates/project.php
+++ site/templates/project.php
@@ -21,14 +21,16 @@
         <div class="main-text">
             <?= $page->maintext()->kt() ?>
             <?php if($page->customBlocks()->isNotEmpty()) : ?>
-                <div class="button-wrapper">
+                <div class="buttons">
                     <?php foreach ($page->customBlocks()->toBlocks() as $block): ?>
-                        <button class="button">
-                            <svg viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M0.852804 1L16.4577 1M16.4577 1L16.6553 17M16.4577 1L0.655273 17"/>
-                            </svg>
-                            <?= $block ?>
-                        </button>
+                        <div class="button-wrapper">
+                            <button class="button">
+                                <svg viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M0.852804 1L16.4577 1M16.4577 1L16.6553 17M16.4577 1L0.655273 17"/>
+                                </svg>
+                                <?= $block ?>
+                            </button>
+                        </div>
                     <?php endforeach ?>
                 </div>
             <?php endif ?>
